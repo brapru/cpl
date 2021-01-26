7007fee0fd73526ae36d4b339444c07996227f5e
brapru <smith.pruitt@gmail.com>
2021-01-25 22:24:30 -0500

    psuedo code for bsearch

--- cpl/binsearch.h
+++ cpl/binsearch.h
@@ -1,5 +1,24 @@
 #include "cpl.h"
 
+/*
+ * max, min
+ *
+ * if min > max:
+ * 	return NOTFOUND;
+ *
+ * get_midpoint
+ * 
+ * if get_midpoint = key ?
+ *	return midpoint;
+ *
+ * if get_midpoint < key
+ * 	binsearch(get_midpoint + 1)
+ * 
+ * if get_midpoint > key
+ * 	binsearch(get_midpoint - 1)
+ *
+ */
+
 static inline void binsearch(void){
 	printf("BSEARCH\n");
 }
